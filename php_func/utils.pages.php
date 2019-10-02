bd2b15676cd2826a2d14a79eb06f7082313174db
r18002537 <thomas.rippol@etu.univ-amu.fr>
2019-10-02 10:05:41 +0200

    Logo github

--- php_func/utils.pages.php
+++ php_func/utils.pages.php
@@ -20,7 +20,7 @@ function my_footer() {
     echo <<<EOT
     <footer class="bottomFooter">
         <p> Copyright 2019 </p>
-        <img src = "img/github.png" alt="LogoGitHub" class="logogithub">
+        <img src = "img/logogithub.png" alt="LogoGitHub" class="logogithub">
     </footer>
 EOT;
 }
