0f24010c5776623b8d1eb0534f13f16d01e7cea4
r18002537 <thomas.rippol@etu.univ-amu.fr>
2019-10-03 11:12:49 +0200

    divconnexion

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
     require_once 'php_func/utils.pages.php';
-    my_header();
+    my_header(); ?>
+
+<div class="divconnexion">
+    <h3>Mon premier forulaire avec AJAX</h3><br>
+    <form method="" id="form" action="">
+        <label>Nom utilisateur :</label><br>
+        <input type="text" name="username">
+        <br><br>
+        <label>Mot de passe :</label><br>
+        <input type="password" name="password">
+        <br><br>
+        <input type="Submit" value="validation">
+    </form>
+</div>
+
+<?php
     mainView();
     my_footer();
+                    ?>
