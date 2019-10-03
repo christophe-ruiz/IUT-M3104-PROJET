a8618de55d6e66c29a7d349d5538229d8628f36a
r18002537 <thomas.rippol@etu.univ-amu.fr>
2019-10-03 11:50:12 +0200

    divconnexion

--- index.php
+++ index.php
@@ -2,21 +2,23 @@
     require_once 'php_func/utils.pages.php';
     my_header(); ?>
 
-<div class="divconnexion">
-    <h3> Connexion </h3><br>
-    <form method="" id="form" action="">
-        <label>Nom utilisateur :</label><br>
-        <input type="text" name="username">
-        <br><br>
-        <label>Mot de passe :</label><br>
-        <input type="password" name="password">
-        <br><br>
-        <a href=""> Mot de passe oublié ? </a>
-        <br><br>
-        <input type="Submit" value="Validation">
+    <div class="logintriangle"></div>
 
-    </form>
-</div>
+    <div class="divconnexion">
+        <h3> Connexion </h3><br>
+        <form method="" id="form" action="">
+            <label>Nom utilisateur :</label><br>
+            <input type="text" name="username">
+            <br><br>
+            <label>Mot de passe :</label><br>
+            <input type="password" name="password">
+            <br><br>
+            <a href=""> Mot de passe oublié ? </a>
+            <br><br>
+            <input type="Submit" value="Validation">
+
+        </form>
+    </div>
 
 <?php
     mainView();
