746ca147ac48b7cee9c2fd2588affdf081975cba
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-10-03 15:16:20 +0200

    db

---
+++ php/credentials.php
@@ -0,0 +1,20 @@
+<?php
+    $dbCredentials = parse_ini_file('../ini/dbCredentials.ini',TRUE)['database'];
+
+    $host = $dbCredentials['host'];
+    $username = $dbCredentials['username'];
+    $password = $dbCredentials['password'];
+    $dbName = $dbCredentials['username'];
+    $charset = 'utf8mb4';
+
+    $dsn = 'mysql:host='.$host.';dbname='.$dbName.';charset='.$charset;
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try {
+        $pdo = new PDO($dsn, $username, $password, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
