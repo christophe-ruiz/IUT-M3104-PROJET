c79f1a620711a24a04cf3791269cbfea01b72105
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-10-03 11:54:49 +0200

    debut inscription

---
+++ php/inscription.php
@@ -0,0 +1,13 @@
+<?php
+    $action = $_POST['action'];
+    $validPWD = true;
+
+    if (isset($action) && !empty($action)) {
+        $login = $_POST['login'];
+        $pwd = $_POST['pwd'];
+        $vpwd = $_POST['vpwd'];
+    }
+
+    if ($pwd != $vpwd) $validPWD = false;
+
+
