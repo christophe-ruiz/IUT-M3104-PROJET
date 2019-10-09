f9416b5442ee463b8629ffffdd21c16a5841390f
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-10-09 12:07:17 +0200

    redirection vers index

--- class/Database.php
+++ class/Database.php
@@ -3,5 +3,27 @@
 
 class Database
 {
+    private static function connect() {
+        $dbCredentials = parse_ini_file('../config/dbCredentials.config',TRUE)['database'];
 
+        $host = $dbCredentials['host'];
+        $username = $dbCredentials['username'];
+        $password = $dbCredentials['password'];
+        $dbName = $dbCredentials['username'];
+        $charset = 'utf8mb4';
+
+        $dsn = 'mysql:host='.$host.';dbname='.$dbName.';charset='.$charset;
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try {
+            $pdo = new PDO($dsn, $username, $password, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+
+        return $pdo;
+    }
 }
