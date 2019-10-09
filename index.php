a3ffd8707313f2b9de446122e10b5b430dbc839b
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-10-09 11:10:46 +0200

    mvc routes

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 spl_autoload_register(function ($class) {
-    if (file_exists('class/' . $class . '.php'))
+    if (file_exists('./class/' . $class . '.php'))
         require_once 'class/' . $class . '.php';
-    else if (file_exists('app/controller/' . $class . '.php'))
+    else if (file_exists('./app/controller/' . $class . '.php'))
         require_once 'app/controller/' . $class . '.php';
 });
 require_once 'php_func/utils.pages.php';
 require_once 'core/routes.php';
+echo 'uh oh';
