6cb564d649c3691b2b0def60c4f9d1f9fce8a409
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-10-09 16:58:25 +0200

    assistance

--- php/assistance.php
+++ php/assistance.php
@@ -1,23 +1,11 @@
 <?php
-    require_once 'php_func/utils.pages.php';
+    require_once 'php_func/utils.pages2.php';
     my_header();
     ?>
-<!DOCTYPE html>
-    <html lang="fr">
-    <head>
 
-        <meta charset="utf-8">
-        <title>FreeNote</title>
-        <link rel="stylesheet" href="{$stylesheet}">
-        <script type="text/javascript" src="../js/showmenu.js"></script>
-
-    </head>
-    <body>
-    <header class="topHeader">
         <div id="deco">
             <a class="deconnexion" onclick="showmenu()"> Deconnexion </a>
         </div>
-    </header>
 
 <?php
     my_footer();
