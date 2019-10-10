e74f833ccf9e71a637fb4cda6118f1d61f0df098
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-10-10 09:24:39 +0200

    Inscription

--- index.php
+++ index.php
@@ -3,4 +3,7 @@
     require_once 'core/autoloader.php';
     require_once 'php_func/utils.pages.php';
     if (!isset($_GET['url'])) require_once 'core/routes.php';
-    else Controller::CreateView($_GET['url']);
+    else {
+        $_GET['url'] = 'index';
+        Controller::CreateView($_GET['url']);
+    }
