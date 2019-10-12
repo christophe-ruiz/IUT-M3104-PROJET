3803919410bacd90f1a3c06cbebd8b6ce4825f26
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-10-12 12:36:22 +0200

    Inscription & routes

--- php/func_inscription.php
+++ php/func_inscription.php
@@ -1,5 +1,6 @@
 <?php
-    require_once '../core/autoloader.php';
+    require '../core/autoloader.php';
+    include '../class/Database.php';
     $action = $_POST['action'];
     if (!isset($action) || empty($action)) die("Erreur Inscription");
     $validPWD = true;
