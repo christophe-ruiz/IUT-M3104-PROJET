c96cb65ba6a3397bf48ac16b38d007ac36cd9091
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-10-12 11:52:47 +0200

    Inscription & routes

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
     require_once 'core/autoloader.php';
     require_once 'php_func/utils.pages.php';
     require_once 'core/routes.php';
-    while (!isset($_GET['url']))
-    if ($_GET['url'] == 'index')
-        Index::CreateView('index');
-    if ($_GET['url'] == 'board')
-        Board::CreateView('board');
-    else {
-        $_GET['url'] = 'index';
-        echo $_GET['url'];
+    while (!isset($_GET['url'])) {
+        if ($_GET['url'] == 'index')
+            Index::CreateView('index');
+        if ($_GET['url'] == 'board')
+            Board::CreateView('board');
+        else {
+            $_GET['url'] = 'index';
+            echo $_GET['url'];
+    }
     }
